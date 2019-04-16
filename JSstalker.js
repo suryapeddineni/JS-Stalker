0847d0a8b6fe867451f0d9b2f4b126848115f90c
SuryaChaitanya Peddineni <43539590+psuryachaitanya@users.noreply.github.com>
2019-04-17 00:08:59 +0530

    Initial commit

---
+++ JSstalker.js
@@ -0,0 +1,8 @@
+
+var fName=prompt("Ask Yours First Name");
+var lName=prompt("Ask Yours Last Name");
+var age=prompt("Ask Yours Age");
+
+console.log( "Your Full name is" + fName);
+console.log("Your Last name is" + lName);
+console.log("Your Age is" + age);
